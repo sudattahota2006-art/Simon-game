62823e33c42fc922fcc87b73a888b405f452a0a6
sudattahota2006-art <sudattahota2006@gmail.com>
2026-02-07 10:29:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+let gameSeq=[];
+let userSeq=[];
+
+let btns=["red","green","yellow","purple"];
+
+let started=false;
+let level=0;
+
+let h2=document.querySelector("h2");
+
+document.addEventListener("click",function() {
+    if(started==false){
+        console.log("Game is started");
+        started=true;
+
+        levelUp();
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout( function () {
+        btn.classList.remove("flash");
+    },250);
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout( function () {
+        btn.classList.remove("userflash");
+    },250);
+}
+
+function levelUp(){
+    userSeq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+    let ranIdx=Math.floor(Math.random() * 3);
+    let randColor=btns[ranIdx];
+    let randBtn=document.querySelector(`.${randColor}`);
+    //console.log(ranIdx);
+    //console.log(randColor);
+    //console.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+function checkAns(idx){
+    if(userSeq[idx]===gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+    }else{
+        h2.innerHTML = `Game over! Your score was <b>${level}</b><br>Click anywhere to start.`;
+        document.querySelector("body").style.backgroundColor="red";
+    setTimeout(function() {
+        document.querySelector("body").style.backgroundColor="white";
+        reset();
+    },150)
+    }
+}
+
+function btnPress(){
+    //console.log(this);
+    let btn=this;
+    userFlash(btn);
+    let userColor=btn.getAttribute("id");
+    userSeq.push(userColor);
+    checkAns(userSeq.length-1);
+}
+
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    started=false;
+    userSeq=[];
+    gameSeq=[];
+    level=0;
+}
